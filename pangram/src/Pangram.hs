39619dac2fa9408f6a7f41f7daba8abb6d72832b
alephsur <david.franco.gutierrez@gmail.com>
2021-10-03 17:32:30 +0200

    sum-of-multiples

--- pangram/src/Pangram.hs
+++ pangram/src/Pangram.hs
@@ -1,15 +1,10 @@
 module Pangram (isPangram) where
 
+import Data.Char ( toLower)
+import Data.List (sort)
+
 isPangram :: String -> Bool
-isPangram text = (Prelude.length $ unique $ filterLetter $ getLowerString $ sortList text) == 26
-
-
-sortList :: [Char] -> [Char]
-sortList [] = []
-sortList (x:xs) = 
-    let small = sortList [a | a <- xs, a <= x]
-        big = sortList [a | a <- xs, a > x]
-    in small ++ [x] ++ big
+isPangram text = (Prelude.length $ unique $ filterLetter $ getLowerString $ sort text) == 26
 
 
 filterLetter :: [Char] -> [Char]
@@ -25,36 +20,3 @@ unique (x:xs) = x:unique (Prelude.filter ((/=) x) xs)
 getLowerString::[Char] -> [Char]
 getLowerString text = 
     map toLower text
-
-toLower :: Char -> Char
-toLower c =
-    case c of        
-        'A' -> 'a'
-        'B' -> 'b'
-        'C' -> 'c'
-        'D' -> 'd'
-        'E' -> 'e'
-        'F' -> 'f'
-        'G' -> 'g'
-        'H' -> 'h'
-        'I' -> 'i'
-        'J' -> 'j'
-        'K' -> 'k'
-        'L' -> 'l'
-        'M' -> 'm'
-        'N' -> 'n'
-        'O' -> 'o'
-        'P' -> 'p'
-        'Q' -> 'q'
-        'R' -> 'r'
-        'S' -> 's'
-        'T' -> 't'
-        'U' -> 'u'
-        'V' -> 'v'
-        'W' -> 'w'
-        'X' -> 'x'
-        'Y' -> 'y'
-        'Z' -> 'z'
-        _ -> c
-
-
---
+++ sum-of-multiples/src/SumOfMultiples.hs
@@ -0,0 +1,24 @@
+module SumOfMultiples (sumOfMultiples) where
+
+import Data.List (nub,sum)
+
+sumOfMultiples :: [Integer] -> Integer -> Integer
+sumOfMultiples factors limit = 
+    Data.List.sum $ nub $ processListToGetMultiples factors limit []
+
+
+processListToGetMultiples :: [Integer] -> Integer -> [Integer] -> [Integer]
+processListToGetMultiples [] limit acc = acc
+processListToGetMultiples factors limit acc = 
+    processListToGetMultiples (tail factors) limit (acc ++ (getMultiplesForNumber (head factors) 0 limit [] ))
+
+
+
+getMultiplesForNumber :: Integer -> Integer -> Integer -> [Integer] -> [Integer]
+getMultiplesForNumber number multiplier limit  acc =
+    if (number * multiplier) < limit then
+        getMultiplesForNumber number (multiplier+1) limit acc ++ [(number * multiplier)]
+    else
+        acc
+    
+
