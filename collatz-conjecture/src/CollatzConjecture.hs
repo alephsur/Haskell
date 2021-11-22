86ff739ff8ed2f4819e76f49b9fb4e433deb79be
alephsur <david.franco.gutierrez@gmail.com>
2021-11-22 19:17:08 +0100

    colla-conjecture
    improve the solution

--- collatz-conjecture/src/CollatzConjecture.hs
+++ collatz-conjecture/src/CollatzConjecture.hs
@@ -2,13 +2,10 @@ module CollatzConjecture (collatz) where
 
 collatz :: Integer -> Maybe Integer
 collatz n = calculate n 0
+  where calculate 0 0 = Nothing
+        calculate 1 acc = Just acc
+        calculate n acc 
+            | even n =  calculate (div n 2) (acc+1)
+            | odd n = calculate (3*n+1) (acc+1)
+            | otherwise = Nothing
 
-
-calculate :: Integer -> Integer -> Maybe Integer
-calculate 0 0 = Nothing
-calculate 1 acc = Just acc
-calculate n acc 
-    | n < 0 = Nothing
-    | even n =  calculate (div n 2) (acc+1)
-    | odd n = calculate (3*n+1) (acc+1)
-    | otherwise = Nothing
