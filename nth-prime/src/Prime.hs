4737c80dc32d7454b4ceecff156545701ca15e4b
alephsur <david.franco.gutierrez@gmail.com>
2022-01-07 18:25:42 +0100

    nth-prime solution

---
+++ nth-prime/src/Prime.hs
@@ -0,0 +1,14 @@
+module Prime (nth) where
+
+nth :: Int -> Maybe Integer
+nth n
+    | n > 0 = Just $ last $ take n primeNumbers
+    |otherwise = Nothing
+
+
+primeNumbers :: [Integer]
+primeNumbers = sieve[2..]
+
+
+sieve :: [Integer] -> [Integer]
+sieve (p:xs) = p : sieve[x | x <- xs, x `mod` p /= 0]
