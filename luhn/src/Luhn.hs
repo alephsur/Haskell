18a2e00d9f1a078e40c8521761772b864d4ac283
alephsur <david.franco.gutierrez@gmail.com>
2022-01-07 17:52:00 +0100

    Luhn improve the version

--- luhn/src/Luhn.hs
+++ luhn/src/Luhn.hs
@@ -8,26 +8,15 @@ import Data.List    (reverse)
 isValid :: String -> Bool
 isValid n
     | length cleanStr == 1 = False
-    | totalSum `mod` 10 == 0 = True
+    | (luhn numberList) `mod` 10 == 0 = True
     | otherwise = False
     where 
-        cleanStr = filter isAlphaNum n
-        str = reverse cleanStr        
-        listDobleNumber = getListOfNumber str 1 ""
-        totalSum = sumDigitsString listDobleNumber
+        cleanStr = reverse $ filter isAlphaNum n
+        numberList = map digitToInt cleanStr
+        luhn (x:y:ys) = x + (if y<5 then y*2 else (y*2)-9) + luhn ys
+        luhn [x] = x
+        luhn [] = 0
 
 
 
-getListOfNumber :: String -> Int -> String -> String
-getListOfNumber [] _ acc = acc
-getListOfNumber (x:xs) i acc
-    | even i = if n*2 > 9 then getListOfNumber xs (i+1) (acc ++ nIndexEvenGT9:[]) else  getListOfNumber xs (i+1) (acc ++ nIndexEvenLT9:[])
-    | otherwise = getListOfNumber xs (i+1) (acc ++ x:[])
-    where 
-        n = digitToInt x
-        nIndexEvenGT9 = intToDigit ((n*2)-9)
-        nIndexEvenLT9 = intToDigit (n*2)
-
-sumDigitsString :: String -> Int
-sumDigitsString str = foldl (\acc x -> acc + (digitToInt x)) 0 str
 
