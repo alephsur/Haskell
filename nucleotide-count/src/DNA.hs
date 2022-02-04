1a010893e9ee8aa0247018d42d621aa93fcdccc2
alephsur <david.franco.gutierrez@gmail.com>
2022-02-04 16:48:24 +0100

    nucletoide-count definitive solution

--- nucleotide-count/src/DNA.hs
+++ nucleotide-count/src/DNA.hs
@@ -9,16 +9,11 @@ data Nucleotide = A | C | G | T deriving (Eq, Ord, Show)
 
 nucleotideCounts :: String -> Either String (Map Nucleotide Int)
 nucleotideCounts xs = 
-    if isLeft mapSequence then
-        Left xs
-    else
-        Right $ union mapedSequence (fromList [(A,0),(C,0),(G,0),(T,0)])
-    where
-        mapSequence = histogram <$> (sequence $ map convert xs)
-        mapedSequence = fromListWith (+) (fromRight [] mapSequence)
-        histogram list =[(x, 1) | x <- list]
+    histogram <$> (traverse convert xs)
 
 
+histogram :: [Nucleotide] -> Map Nucleotide Int
+histogram list = fromListWith (+) $ [(x, 1) | x <- list]
 
 convert :: Char -> Either String Nucleotide
 convert 'A' = Right A
