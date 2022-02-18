586d652033e2b4a494a60bc25ad164c37f0a7d75
alephsur <david.franco.gutierrez@gmail.com>
2022-02-18 17:24:54 +0100

    SumOfMultiples
    final solution

--- sum-of-multiples/src/SumOfMultiples.hs
+++ sum-of-multiples/src/SumOfMultiples.hs
@@ -4,15 +4,9 @@ import Data.List (nub,sum, concat)
 
 sumOfMultiples :: [Integer] -> Integer -> Integer
 sumOfMultiples [] limit = 0
-sumOfMultiples factors limit = 
-    Data.List.sum $ nub $ concat (processListToGetMultiples factors limit)
-
+sumOfMultiples factors limit = Data.List.sum . nub . concatMap (getMultiplesForNumber limit) $ factors
 
 
 getMultiplesForNumber :: Integer -> Integer -> [Integer]
-getMultiplesForNumber 0 limit = [0]
-getMultiplesForNumber number limit = takeWhile (<limit) [number*i |i <- [1..]]
-
-
-processListToGetMultiples :: [Integer] -> Integer -> [[Integer]]
-processListToGetMultiples factors limit = [(getMultiplesForNumber i limit) | i <- factors]
+getMultiplesForNumber limit 0  = [0]
+getMultiplesForNumber limit number = takeWhile (<limit) [number*i |i <- [1..]]
