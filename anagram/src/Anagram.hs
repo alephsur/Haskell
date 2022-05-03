dea55cb6dd784d0f58fc804648249ee52dc3e646
alephsur <david.franco.gutierrez@gmail.com>
2022-05-03 19:01:58 +0200

    Anagram
    Second iteration

--- anagram/src/Anagram.hs
+++ anagram/src/Anagram.hs
@@ -10,20 +10,17 @@ anagramsFor xs xss = processListOfWords xs xss []
 
 
 processListOfWords :: String -> [String] -> [String] -> [String]
-processListOfWords _ [] result = result
+processListOfWords _ [] result = reverse result
 processListOfWords xs (x:xss) result
-    | isRight $ compareWords xs x = processListOfWords xs xss (result ++ [x])
+    | compareWords xs x = processListOfWords xs xss (x : result)
     | otherwise = processListOfWords xs xss result
 
 
-compareWords :: String -> String -> Either String String
+compareWords :: String -> String -> Bool
 compareWords word1 word2 =            
-    if comparison == True && comparisonEquals == False then        
-        Right word2
-    else 
-        Left word2
+    comparison && not comparisonEquals    
     where 
-        comparison = (sort (map toUpper word1)) == (sort (map toUpper word2))
-        comparisonEquals = (map toUpper word1) == map toUpper word2
+        comparison = sort (map toUpper word1) == sort (map toUpper word2)
+        comparisonEquals = map toUpper word1 == map toUpper word2
 
 
