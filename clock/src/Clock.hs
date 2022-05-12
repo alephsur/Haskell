ef71b59aacb940a34e276eef1e16f629dc259707
alephsur <david.franco.gutierrez@gmail.com>
2022-05-12 21:58:46 +0200

    Clock
    second iteration

--- clock/src/Clock.hs
+++ clock/src/Clock.hs
@@ -11,20 +11,19 @@ fromHourMin :: Int -> Int -> Clock
 fromHourMin hour min = 
   Clock hourCalculated minutesCalculate
   where
-    hourModulated = (if hour < 0 then 24 + (hour `mod` 24) else hour `mod` 24)
-    hourMinutesNegativeRollOver = (if min < 0 || min >= 60 then min `div` 60 else 0)
+    hourMinutesNegativeRollOver = min `div` 60
     minutesCalculate = min `mod` 60
-    hourCalculated = (hourModulated + hourMinutesNegativeRollOver) `mod` 24
+    hourCalculated = (hour + hourMinutesNegativeRollOver) `mod` 24
     
 
 toString :: Clock -> String
 toString clock = (showIntPad (hour clock)) ++ ":" ++ (showIntPad (minutes clock))
 
 addDelta :: Int -> Int -> Clock -> Clock
-addDelta hour min clock = fromHourMin addedHours addedMinutes
+addDelta h m clock = fromHourMin addedHours addedMinutes
   where 
-    addedHours = ((Clock.hour clock) + hour)
-    addedMinutes = ((Clock.minutes clock) + min)
+    addedHours = ((hour clock) + h)
+    addedMinutes = ((minutes clock) + m)
 
 
 showIntPad :: Int -> String
