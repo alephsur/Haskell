a3c08cbd8f6e8f14e8a1fd1e8eb6d0a6261af80e
alephsur <david.franco.gutierrez@gmail.com>
2022-11-12 19:13:05 +0100

    last improvement

--- pascals-triangle/src/Triangle.hs
+++ pascals-triangle/src/Triangle.hs
@@ -1,30 +1,11 @@
 module Triangle (rows) where
 
 rows :: Int -> [[Integer]]
-rows x = generateRows x
+rows x = take x $ iterate generateNextRow [1]
 
 
-generateRows :: Int -> [[Integer]]
-generateRows n
-    | n == 0 = []
-    | n == 1 = [[1]]
-    | n == 2 = [[1],[1,1]]
-    | otherwise = (generateRows 1) ++ partialTriangle
-    where 
-        partialTriangle = take (n-1) $ iterate (generateTriangle' 3) [1,1]
-
-
-generateTriangle' :: Int -> [Integer] -> [Integer]
-generateTriangle' rowNumber previousRow = 
-    generateNextRow' previousRow 0 []
-
-
-generateNextRow' :: [Integer] -> Int -> [Integer] -> [Integer]
-generateNextRow' previousRow position nextRow
-    | position == 0 = generateNextRow' previousRow (position +1) (nextRow ++ [1])
-    | position == (rowNumber -1) = generateNextRow' previousRow (position +1) (nextRow ++ [1])
-    | position == rowNumber = nextRow
-    | otherwise = generateNextRow' previousRow (position +1) (nextRow ++ [nextNumber])
-    where 
-        nextNumber = (previousRow !! position) + (previousRow !! (position -1))
-        rowNumber = (length previousRow) + 1 
+generateNextRow :: [Integer] -> [Integer]
+generateNextRow row = zipWith (+) zipRow1 zipRow2
+    where
+        zipRow1 = [0] ++ row
+        zipRow2 = row ++ [0]
